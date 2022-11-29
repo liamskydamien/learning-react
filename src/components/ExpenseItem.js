1dc6f47e2178963849b5b59a0f6666a0914957b9
liamskydamien <liamskydamien@gmail.com>
2022-11-29 16:33:31 +0100

    Adding dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,10 +7,10 @@ function ExpenseItem(){
 
     return (
         <div className="expense-item">
-            <div>{expenseDate}</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
                 <h2>{expenseName}</h2>
-                <div className="expense-item__price">{expenseCost}</div>
+                <div className="expense-item__price">{expenseCost}$</div>
             </div>
         </div>
     )
