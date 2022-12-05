82a646f2a768ac89ce5193ce96b4c362a2723a64
liamskydamien <liamskydamien@gmail.com>
2022-12-05 18:48:52 +0100

    Adding Child-to-Parent Communication for a Filter

--- src/components/ExpensesAndIncomes/ExpensesAndIncome.js
+++ src/components/ExpensesAndIncomes/ExpensesAndIncome.js
@@ -2,9 +2,17 @@ import ExpenseItem from "./ExpenseItem";
 import IncomeItem from "./IncomeItem";
 import './ExpensesAndIncome.css'
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import {useState} from "react";
 
 function ExpensesAndIncome(props){
+    const [filteredYear, setFilteredYear] = useState('2022');
+    const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+      console.log(selectedYear);
+    };
     return <Card className="expensesAndIncome">
+        <ExpenseFilter onFilterChange={filterChangeHandler}></ExpenseFilter>
         <ExpenseItem name={props.expenses[0].title}
                      date = {props.expenses[0].date}
                      cost = {props.expenses[0].amount}>
