3f9661f500e0ad3a26e72a64355e59891c257464
liamskydamien <liamskydamien@gmail.com>
2022-12-11 14:47:55 +0100

    Addind different Colors for spending or income

--- src/components/ExpensesAndIncomes/ExpenseItem.js
+++ src/components/ExpensesAndIncomes/ExpenseItem.js
@@ -3,12 +3,17 @@ import DateComponent from "./DateComponent";
 import ItemCard from "./UI/ItemCard";
 
 function ExpenseItem(props){
+    const color = props.cost > 0 ? '#097501' : '#bd1600';
+    const pre = props.cost > 0 ? '+' : '';
     return (
         <ItemCard>
             <DateComponent date = {props.date}></DateComponent>
             <div className="expense-item__description">
                 <h2>{props.name}</h2>
-                <div className="expense-item__price">{props.cost}$</div>
+                <div className="expense-item__price" style={
+                    {backgroundColor: color}}>
+                    {pre + props.cost}$
+                </div>
             </div>
         </ItemCard>
     )
